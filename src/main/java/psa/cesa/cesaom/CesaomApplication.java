4d8a453a51be1ebf55237506c072a614ce9f62a2
psa <psa@psa>
2019-11-20 13:16:09 +0100

    Initial commit

---
+++ src/main/java/psa/cesa/cesaom/CesaomApplication.java
@@ -0,0 +1,13 @@
+package psa.cesa.cesaom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CesaomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CesaomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/psa/cesa/cesaom/CesaomApplicationTests.java
@@ -0,0 +1,13 @@
+package psa.cesa.cesaom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CesaomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
